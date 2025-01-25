6f762ac2140d232ba2dddfbc1bb788e11c1c9d0d
OUTOFTHEBLUE\whate <whateverfloatsyourboat077@gmail.com>
2025-01-25 18:14:30 -0500

    Finished Clourd in Background code. All Pieces move from 1 edge and appear again.

---
+++ Assets/Scripts/Cloud Body Movement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBodymoevment : MonoBehaviour
+{
+    public float speed = 0.005f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        pos.x += speed;
+
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(pos);
+
+        if (screenPos.x > Screen.width + 25)
+        {
+            pos.x = -15;
+        }
+
+        transform.position = pos;
+
+    }
+}
+
