1c9e9477085d5d8eba7231f6e55e7724e317dd07
Richard Murillo <murillri@shernet.sheridancollege.ca>
2025-01-29 12:12:18 -0500

    Lurp Idek

---
+++ Assets/Scripts/LrupDemo.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LrupDemo : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float t;
+    public AnimationCurve curve;
+
+    public Vector2 start;
+    public Vector2 end;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector2 scale = transform.localScale;
+        //scale.x = Mathf.Lerp(0, 1, t);
+
+        transform.position = Vector2.Lerp(start, end, curve.Evaluate(t));
+    }
+}
